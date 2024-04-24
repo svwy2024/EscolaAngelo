b2342e9f9421ba3a5cf04d5e49945335c0e22a98
Sarah Vitória <162985938+svwy2024@users.noreply.github.com>
2024-04-24 08:10:31 -0300

    Add files via upload

---
+++ Wy-SV 24_04_24/dicionario.py
@@ -0,0 +1,26 @@
+listaNome = []
+
+
+listaNome.append("Edson")
+listaNome.append("Sarinhaah")
+listaNome.append("wendy")
+
+print(listaNome)
+print(listaNome[1])
+
+for nome in listaNome:
+    print(nome)
+
+#DICIONARIO
+
+primeiroDicionario = {}
+
+primeiroDicionario['nome'] = "Sarah"
+primeiroDicionario['tipo'] = "Aluna sarinha"
+primeiroDicionario["numero"] = 9
+print(primeiroDicionario)
+print(primeiroDicionario["tipo"])
+
+#Anatomia
+#lista[indice] = valor
+#dicionario[chave] = valor
